3c7bf7ac2522471b1d88f0e3c48350db5ed66b14
dilshod-gaynazarov <dilshod7861@gmail.com>
2025-07-30 11:51:13 +0500

    log

--- src/main.js
+++ src/main.js
@@ -1,31 +1,10 @@
-import express from 'express';
+import { application } from './app.js';
 import config from './config/index.js';
-import { connectDB } from './db/index.js';
-import router from './routes/index.routes.js';
-import cookieParser from 'cookie-parser';
-import { globalErrorHandle } from './error/global-error-handle.js';
-import helmet from 'helmet';
-import cors from 'cors';
-import { join } from 'path';
+import express from 'express';
 
 const app = express();
 const PORT = config.PORT || 2000;
 
-app.use(cors({
-    origin: '*'
-}));
-app.use(helmet());
-
-app.use(express.json());
-
-app.use(cookieParser());
-
-app.use('/api/uploads', express.static(join(process.cwd(), '../uploads')));
-
-await connectDB();
-
-app.use('/api', router);
-
-app.use(globalErrorHandle);
+await application(app);
 
 app.listen(PORT, () => console.log('Server running on port', PORT));
